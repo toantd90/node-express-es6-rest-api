82f3dc3b1d048fc0283e6bf846850184c1b8994f
PRDCV206-U <toantdfu@gmail.com>
2017-06-08 17:08:13 +0700

    Fixing authentication issue

--- api/authenticate.js
+++ api/authenticate.js
@@ -1,16 +1,45 @@
 import User from '../models/user'
-import {Router} from 'express'
+import express, {Router} from 'express'
+import jwt from 'jsonwebtoken'
 
-let authenticate = (req, res, next) => {
-  let api = Router()
-  api.route('/')
-  .all((req, res, next) => {
-    next()
+let authenticate = () => {
+  const app = express()
+  const api = Router()
+
+  // Route to authenticate a user
+  api.post('/', (req, res) => {
+    // Find the user
+    User.findOne({
+      username: req.body.username
+    }, (err, user) => {
+      if (err) {
+        throw err
+      }
+      if (!user) {
+        console.error(`Authentication failed. User '${req.body.username}' not found.`);
+        res.json({success: false, message: 'Authentication failed. User not found.'})
+      } else {
+        // Check if password matches
+        if (user.password !== req.body.password) {
+          console.error(`Authentication failed. Wroong password for user '${req.body.username}'`);
+          res.json({success: false, message: 'Authentication failed. Wrong password.'})
+        } else {
+          // If user is founded and password is right
+          // create a token
+          let token = jwt.sign(user, app.get('superSecret'), { expiresInMinutes: 720 })
+
+          // return the information including token as JSON
+          res.json({
+            success: true,
+            message: 'Enjoy your token',
+            token: token
+          })
+        }
+      }
+    })
   })
-  .post((req, res, next) => {
-    res.json({message: 'POST'})
-    next()
-  })
+
+  return api
 }
 
-export default() => authenticate
+export default authenticate
--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 import {Router} from 'express'
-import users from './users'
 import authenticate from './authenticate'
 
 export default() => {
@@ -8,11 +7,13 @@ export default() => {
   let api = Router()
 
   // mount the facets resource
-  api.use('/users', users())
+  // api.use('/users', users())
   api.use('/authenticate', authenticate())
 
   // perhaps expose some API metadata at the root
-  api.get('/', (req, res) => {})
+  // api.get('/', (req, res) => {
+  //   res.end()
+  // })
 
   return api
 }
